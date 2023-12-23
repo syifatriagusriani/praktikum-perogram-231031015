ee538abbcf8d42003d704ecd1ee77bc09c145fbb
syifatriagusriani <154118734+syifatriagusriani@users.noreply.github.com>
2023-12-23 09:58:19 +0800

    Delete 231031015-syifa-tugas1-a10.py

--- 231031015-syifa-tugas1-a10.py
+++
@@ -1,9 +0,0 @@
-# Input bilangan X dari pengguna
-X = int(input("Masukkan bilangan X: "))
-
-# Cek apakah bilangan X ganjil atau tidak
-if X % 2 != 0:
-    print(f"{X} adalah bilangan Ganjil")
-else:
-    print(f"{X} adalah bilangan Bukan Ganjil")
-
